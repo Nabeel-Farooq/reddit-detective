08754afe1edf0443a2c1be79e8e7d1e398e45b49
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-21 18:48:41 +0500

    Update test_relationships.py

--- tests/test_relationships.py
+++ tests/test_relationships.py
@@ -1,57 +1,102 @@
-from reddit_detective.data_models import Redditor, Subreddit
-from reddit_detective.relationships import Comments, CommentsReplies, Submissions
+import pytest
+
+from reddit_detective.data_models import (
+    Redditor,
+    Subreddit,
+)
+
+from reddit_detective.relationships import (
+    Comments,
+    CommentsReplies,
+    Submissions,
+)
+
 from tests import api_
 
 """
-    Constraint codes to be run when manually testing in Neo4j:
-    
-    CREATE CONSTRAINT UniqueRedditor
-    ON (r:Redditor) ASSERT (r.id) IS UNIQUE
-    
-    CREATE CONSTRAINT UniqueSubmission
-    ON (sm:Submission) ASSERT (sm.id) IS UNIQUE
-    
-    CREATE CONSTRAINT UniqueSubreddit
-    ON (sr:Subreddit) ASSERT (sr.id) IS UNIQUE
+Neo4j constraints for manual testing:
+
+CREATE CONSTRAINT UniqueRedditor
+ON (r:Redditor) ASSERT (r.id) IS UNIQUE
+
+CREATE CONSTRAINT UniqueSubmission
+ON (sm:Submission) ASSERT (sm.id) IS UNIQUE
+
+CREATE CONSTRAINT UniqueSubreddit
+ON (sr:Subreddit) ASSERT (sr.id) IS UNIQUE
 """
 
-
-def test_submissions():
-    sub = Subreddit.from_base_obj(api_.subreddit("learnpython"), 2)
-    submissions_sub = Submissions(sub)
-    assert submissions_sub.code()
-    red = Redditor.from_base_obj(api_.redditor("Anub_Rekhan"), 2)
-    submissions_red = Submissions(red)
-    assert submissions_red.code()
+TEST_SUBREDDIT = "learnpython"
+TEST_REDDITOR = "Anub_Rekhan"
+TEST_LIMIT = 2
 
 
-def test_comments():
-    sub = Subreddit.from_base_obj(api_.subreddit("learnpython"), 2)
-    comments_sub = Comments(sub)
-    assert comments_sub.comments()
-    assert comments_sub.code()
-    red = Redditor.from_base_obj(api_.redditor("Anub_Rekhan"), 2)
-    comments_red = Comments(red)
-    assert comments_red.comments()
-    assert comments_red.code()
+@pytest.fixture(scope="module")
+def subreddit():
+    return Subreddit.from_base_obj(
+        api_.subreddit(TEST_SUBREDDIT),
+        TEST_LIMIT,
+    )
 
 
-def test_replies():
-    sub = Subreddit.from_base_obj(api_.subreddit("learnpython"), 2)
-    replies_sub = CommentsReplies(sub)
-    assert replies_sub.comments()
-    assert replies_sub.code()
-    red = Redditor.from_base_obj(api_.redditor("Anub_Rekhan"), 2)
-    replies_red = CommentsReplies(red)
-    assert replies_red.comments()
-    assert replies_red.code()
+@pytest.fixture(scope="module")
+def redditor():
+    return Redditor.from_base_obj(
+        api_.redditor(TEST_REDDITOR),
+        TEST_LIMIT,
+    )
 
 
-def run():
-    test_submissions()
-    test_comments()
-    test_replies()
+def assert_valid_code(code):
+    assert code is not None
+    assert isinstance(code, list | str)
+
+    if isinstance(code, list):
+        assert len(code) > 0
 
 
-if __name__ == '__main__':
-    run()
+def test_submissions(subreddit, redditor):
+    submissions_sub = Submissions(subreddit)
+    submissions_red = Submissions(redditor)
+
+    assert_valid_code(submissions_sub.code())
+    assert_valid_code(submissions_red.code())
+
+
+def test_comments(subreddit, redditor):
+    comments_sub = Comments(subreddit)
+    comments_red = Comments(redditor)
+
+    assert comments_sub.comments() is not None
+    assert comments_red.comments() is not None
+
+    assert_valid_code(comments_sub.code())
+    assert_valid_code(comments_red.code())
+
+
+def test_replies(subreddit, redditor):
+    replies_sub = CommentsReplies(subreddit)
+    replies_red = CommentsReplies(redditor)
+
+    assert replies_sub.comments() is not None
+    assert replies_red.comments() is not None
+
+    assert_valid_code(replies_sub.code())
+    assert_valid_code(replies_red.code())
+
+
+def test_generated_code_contains_merge(subreddit):
+    submissions = Submissions(subreddit)
+
+    code = submissions.code()
+
+    if isinstance(code, list):
+        joined_code = " ".join(code)
+    else:
+        joined_code = code
+
+    assert "MERGE" in joined_code or "CREATE" in joined_code
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
